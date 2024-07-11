b7a03250d2a325e4315990678a2121ab9310ec02
Oscar Garcia <oscaregarcialeon@gmail.com>
2024-07-11 11:49:39 -0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+function Home() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="name"></label>
+        <input type="text" id="name" name="name" />
+      </form>
+    </div>
+  )
+}
+
+export default Home
